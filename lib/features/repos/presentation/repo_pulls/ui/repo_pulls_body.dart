56ea651faa0b6b89bc43525db400185ab89f5d8c
lukasz.kwiotek <lukasz.kwiotek@euvic.pl>
2024-03-11 11:25:08 +0100

    Pull requests screen ui polishing

--- lib/features/repos/presentation/repo_pulls/ui/repo_pulls_body.dart
+++ lib/features/repos/presentation/repo_pulls/ui/repo_pulls_body.dart
@@ -1,5 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 import '../../../../../translations/translations.gl.dart';
 import '../../../domain/models/pull_model.dart';
@@ -14,17 +14,49 @@ class RepoPullsBody extends StatelessWidget {
       ? ListView.separated(
           padding: const EdgeInsets.only(top: 10),
           itemCount: pulls.length,
-          itemBuilder: (context, index) =>
-              _buildRepoItem(context, pulls[index]),
+          itemBuilder: (context, index) => _buildItem(context, pulls[index]),
           separatorBuilder: (context, index) => const SizedBox(height: 10),
         )
       : Text(LocaleKeys.repo_pulls_empty.tr());
 
-  Widget _buildRepoItem(BuildContext context, PullModel pull) => Row(
-        children: [
-          Text(pull.number.toString()),
-          const SizedBox(width: 20),
-          Text(pull.title),
-        ],
+  Widget _buildItem(BuildContext context, PullModel pull) => Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.blueGrey),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                const Icon(
+                  Icons.rebase_edit,
+                  size: 20,
+                  color: Colors.green,
+                ),
+                const SizedBox(width: 10),
+                Flexible(
+                  child: Text(
+                    pull.title,
+                    style: Theme.of(context).textTheme.titleSmall,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              children: [
+                Text('#${pull.number}'),
+                const SizedBox(width: 10),
+                Text(
+                  '${LocaleKeys.repo_pulls_created_at.tr()} ${DateFormat.yMd().format(pull.createdAt)}',
+                ),
+              ],
+            ),
+          ],
+        ),
       );
 }
